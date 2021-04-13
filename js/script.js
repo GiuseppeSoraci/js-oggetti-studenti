b1db7bf4ade81757ca174d00b2a1474bb9c41e8c
GiuseppeSoraci <78480251+GiuseppeSoraci@users.noreply.github.com>
2021-04-13 17:07:28 +0200

    Array object students

--- js/script.js
+++ js/script.js
@@ -9,15 +9,36 @@ Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostr
 
 $(document).ready(function () {
 
-    //Object student
+    // Object student
     var student = {
-        name: "Paolo",
-        surname: "Bonolis",
+        name: "Dwayne",
+        surname: "Onfroy",
         age: "21",
     }
 
     for (var key in student) {
         console.log(key, student[key]);
+    }
+
+    // Array object students
+    var students = [
+        {
+            name: "John",
+            surname: "Lennon",
+            age: 40,
+        }, {
+            name: "Kurt",
+            surname: "Cobain",
+            age: 27,
+        }, {
+            name: "Jimi",
+            surname: "Hendrix",
+            age: 27,
+        }
+    ]
+
+    for (var i = 0; i < students.length; i++) {
+        console.log(students[i].name, students[i].surname);
     }
 
 });
